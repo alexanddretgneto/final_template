c0e6ec159bae8478dc5a6479b9d6563b5ad7b122
alexanddretgneto <alexanddretgneto@gmail.com>
2023-08-20 01:51:29 -0300

    codigo parcialmente complet

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -183,6 +183,4 @@ def show_exam_result(request, course_id, submission_id):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
-
-
 
